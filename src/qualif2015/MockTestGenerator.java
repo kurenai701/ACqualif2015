a388fa3a72ce50dd55d0e1b259fee078798836cf
kurenai701 <kurenai701@gmail.com>
2015-03-12 02:28:09 +0100

    forgot to add MockTestGenerator

---
+++ src/qualif2015/MockTestGenerator.java
@@ -0,0 +1,29 @@
+package qualif2015;
+
+public class MockTestGenerator {
+
+	
+	
+	
+	public ProblemModel getProblemModTestAlgo()
+	{
+		ProblemModel res = new ProblemModel();
+		
+		// CAS DE TEST
+		res.intTest1 = 42;
+		res.stringTest = "bbb";
+			
+		return res;
+	}
+	
+	// TO Initialize TEST
+		public OutputModel getOutputModelTest()
+		{
+			OutputModel outMod = new OutputModel();
+			outMod.OutputInt = 42;
+			outMod.OtherInt = 1337;
+			return outMod;
+		}
+	
+	
+}
