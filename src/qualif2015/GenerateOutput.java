beb8c23e5141715762ccd966b070a73f47eb84fe
kurenai701 <kurenai701@gmail.com>
2015-03-12 20:55:04 +0100

    generate output (write file algo) tested on simple case

--- src/qualif2015/GenerateOutput.java
+++ src/qualif2015/GenerateOutput.java
@@ -1,6 +1,7 @@
 package qualif2015;
 
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class GenerateOutput {
 
@@ -10,17 +11,23 @@ public class GenerateOutput {
 		GenerateOutput go = new GenerateOutput();
 		MockTestGenerator mock = new MockTestGenerator();
 		
+		ReadInput ri = new ReadInput();
+		
+		Scanner scanInput = ri.ScannerInputFileForUnitTest();
+		ProblemModel pbMod = ri.ProcessReadInputToModel(scanInput);
+		
 		// Obtain output model.
 	
-	//	OutputModel outModTest = mock.getOutputModelTest();
-	//	go.GenerateOutputFileFromOutputModel(outModTest, CommonStatic.OutputGeneratedPathUnitTest);
+		ServerAllocation[] serverAllocation = mock.getServerAllocationTest();
+		int[][] roomMapInProgress = new int[2][5];
+				
+		OutputModel outModTest = new OutputModel(serverAllocation, roomMapInProgress, pbMod);	
+		go.GenerateOutputFileFromOutputModel(outModTest, CommonStatic.OutputGeneratedPathUnitTest);
 	}	
 
 	public void GenerateOutputFileFromOutputModel(OutputModel outModel, String filePath)
 	{
 		System.out.println("GenerateOutputFileFromOutputModel");
-	
-
 		if (outModel == null)
 		{
 			System.out.println("outModel null");
@@ -34,9 +41,29 @@ public class GenerateOutput {
 				// TODO CODE HERE Output Generation Logic
 				//  !!!!!!!!!!!!!!!!!!! //
 				writer.println("Test Generate Output");
-				//writer.println(outModel.OutputInt);
-				//writer.println(outModel.OtherInt);
 				
+
+				int row;
+				int col;
+				int group;
+				
+				for (int ss = 0; ss < outModel.serverAllocation.length; ss++)
+				{			
+					row = outModel.serverAllocation[ss].Row;
+					col = outModel.serverAllocation[ss].Column;
+					group = outModel.serverAllocation[ss].Group;
+					
+					if (row == -1)
+					{
+						System.out.println("x");
+						writer.println("x");
+					}
+					else
+					{
+						System.out.println(row + " " + col + " " + group);
+						writer.println(row + " " + col + " " + group);	
+					}
+				}
 				//  !!!!!!!!!!!!!!!!!!! //
 				
 				writer.close();
--- src/qualif2015/MockTestGenerator.java
+++ src/qualif2015/MockTestGenerator.java
@@ -26,4 +26,19 @@ public class MockTestGenerator {
 	//	}
 	
 	
+	public ServerAllocation[] getServerAllocationTest()
+	{
+		ServerAllocation[] serverAllocation = new ServerAllocation[5];	
+		
+		serverAllocation[0] = new ServerAllocation(0, 0, 1, 0);
+		serverAllocation[1] = new ServerAllocation(1, 1, 0, 1);
+		serverAllocation[2] = new ServerAllocation(2, 1, 3, 0);
+		serverAllocation[3] = new ServerAllocation(3, 0, 4, 1);
+		serverAllocation[4] = new ServerAllocation(4, -1, 42, 42);
+		
+		return serverAllocation;
+		
+	}
+	
+	
 }
