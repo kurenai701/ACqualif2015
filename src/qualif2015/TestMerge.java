1db6b7a7ca37b05580b28a7a3c554e9a111cc6fd
kurenai701 <kurenai701@gmail.com>
2015-03-12 23:35:30 +0100

    wesxrdctfvygbuhnijp,ok

--- src/qualif2015/TestMerge.java
+++ src/qualif2015/TestMerge.java
@@ -2,6 +2,9 @@ package qualif2015;
 
 public class TestMerge {
 
-	public TestMerge(){}
+	
+	///bla
+	public TestMerge(){/*truc*/}
 	// Je fous que des commentaires car c'est pour tester e-git
+//bouh bouh
 }
